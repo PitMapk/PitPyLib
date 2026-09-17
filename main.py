6c98077ff80fa0d668203bac2fbcb928de153b49
Mapk <frpit@mail.ru>
2026-09-18 00:11:35 +0300

    coding 18-09

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import sys
 sys.path.append("PitPyLib")
 from PitPyLib import PitRootClass
 
+class PitAppClass(PitRootClass):
+    pass
+
 if __name__ == '__main__':
-    MyClass = PitRootClass()
+    MyClass = PitAppClass
     MyClass.PitOut('Hello Pit from class!..')
     MyClass.PitOut(sys.version)
